6c11865f0dd45049c7d22948d33a5d78df2a562e
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-01 15:52:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,8 @@ require_relative "user"
 
 class Student < User
 
-  # def initialize(first_name, last_name)
-  #   @first_name = first_name
-  #   @last_name = last_name
-  # end
+  def initialize
+    @knowledge = []
+  end
 
 end
